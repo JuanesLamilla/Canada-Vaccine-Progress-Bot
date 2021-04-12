4179dadf17fc7df00ff789fe0c3028018893d1f7
Juan Lamilla <juan.e.lamilla@gmail.com>
2021-04-11 21:36:23 -0300

    Handle saving plot image

--- CVP-bot.R
+++ CVP-bot.R
@@ -43,48 +43,63 @@ dat <- suppressWarnings(transform(dat, numtotal_1dose = as.numeric(numtotal_1dos
 dates <- rev(unique(dat$week_end))
 
 
-# Get latest Canada wide percent
-latest_percent_first <- round(dat[dat$prename == "Canada" & dat$week_end == dates[1], ]$proptotal_atleast1dose, digits = 0)
-latest_percent_both <- round(dat[dat$prename == "Canada" & dat$week_end == dates[1], ]$proptotal_2doses, digits = 0)
-
-
-
-df <- data.frame(
-  "Received first dose" = latest_percent_first,
-  "Received both doses" = latest_percent_both, check.names = FALSE)
-
-df <- df %>%
-  gather(key, val) %>%
-  mutate(
-    key = factor(key, rev(unique(key))),
-    Total = 100)
-
-
 format_value <- function(key, val) {
   sprintf(
-    "%s - %d%%",
+    "%s: %d%%",
     key, val)
 }
 
+plot_dose_percentages <- function(prv_name) {
 
-ggplot(df, aes(key, val)) +
-  geom_col(fill = "green") +
-  geom_col(aes(y = Total), alpha = 0.5, colour = "black") +
-  geom_text(
-    aes(y = 5, label = format_value(key, val)),
-    hjust = 0,
-    fontface = "bold",
-    colour = "white") +
-  coord_flip() +
-  theme_minimal() +
-  theme(
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank())
+  
+  
+  # Get latest percent wide percent
+  latest_percent_first <- round(dat[dat$prename == prv_name & dat$week_end == dates[1], ]$proptotal_atleast1dose, digits = 0)
+  latest_percent_both <- round(dat[dat$prename == prv_name & dat$week_end == dates[1], ]$proptotal_2doses, digits = 0)
+  
+  
+  
+  df <- data.frame(
+    "Received first dose" = latest_percent_first,
+    "Received both doses" = latest_percent_both, check.names = FALSE)
+  
+  df <- df %>%
+    gather(key, val) %>%
+    mutate(
+      key = factor(key, rev(unique(key))),
+      Total = 100)
+  
+  
+  ggplot(df, aes(key, val)) +
+    geom_col(fill = "green") +
+    geom_col(aes(y = Total), alpha = 0.5, colour = "black") +
+    geom_text(
+      aes(y = 5, label = format_value(key, val)),
+      hjust = 0,
+      fontface = "bold",
+      colour = "white",
+      cex = 13) +
+    coord_flip() +
+    theme_minimal() +
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank())
+  
+}
 
+jpeg(file="plot.jpeg",
+     width=1200 , height=675)
+plot_dose_percentages("Alberta")
+dev.off()
+
+if (file.exists("plot.jpeg")) {
+  #Delete file if it exists
+  file.remove("plot.jpeg")
+}
 
 
 
